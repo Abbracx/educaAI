d42785d762a90b9cb5a354070a63b9170358df65
Abbracx <tankoraphael@gmail.com>
2023-07-09 12:39:07 +0100

    The actions performed are:
    1. Updated the urls-conf file for routing
    2. Added templates for login and logout

--- educaAI/urls.py
+++ educaAI/urls.py
@@ -1,4 +1,4 @@
-"""educaAI URL Configuration
+"""educa URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -15,7 +15,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
+    path('accounts/login/', auth_views.LoginView.as_view(),
+          name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(),
+          name='logout'),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
